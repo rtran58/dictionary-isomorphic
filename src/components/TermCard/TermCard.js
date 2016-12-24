055f15879bafffbb54ff62887f667902a4e74e2d
Ricky Tran <rtran58@stanford.edu>
2016-12-24 13:35:51 -0800

    Implement card toggle

--- src/components/TermCard/TermCard.js
+++ src/components/TermCard/TermCard.js
@@ -1,16 +1,41 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import cx from 'classnames';
 import s from './TermCard.scss';
 
-const TermCard = ({ className, term }) => (
-  <div className={cx(s.root, className)}>
-    <div className={s.container}>
-      <div className={s.word}>{term.word}</div>
-      <div className={s.definition}>{term.definition}</div>
-    </div>
-  </div>
-);
+class TermCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showDefinition: false,
+    };
+
+    this.toggleDefinition = this.toggleDefinition.bind(this);
+  }
+
+  toggleDefinition() {
+    debugger
+    this.setState({ showDefinition: !this.state.showDefinition });
+  }
+
+  render() {
+    const word = <span className={s.word}>{this.props.term.word}</span>;
+    const definition = <span className={s.definition}>{this.props.term.definition}</span>
+    const cardBody = this.state.showDefinition
+      ? <div>{word} - {definition}</div>
+      : <div>{word}</div>;
+
+    return (
+      <div className={cx(s.root, this.props.className)}
+           onClick={this.toggleDefinition} >
+        <div className={s.container} >
+          {cardBody}
+        </div>
+      </div>
+    );
+  }
+}
 
 TermCard.propTypes = {
   className: PropTypes.string,
