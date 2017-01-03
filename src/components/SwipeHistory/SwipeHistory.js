26d8256967e1b4d80b75cab067c9c3fd881ac949
Ricky Tran <rtran58@stanford.edu>
2017-01-02 19:31:26 -0800

    Implement toggle to show the definition for all words

--- src/components/SwipeHistory/SwipeHistory.js
+++ src/components/SwipeHistory/SwipeHistory.js
@@ -1,18 +1,41 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './SwipeHistory.scss';
 import cx from 'classnames';
 
-const SwipeHistory = ({ className, entries }) => (
-    <div className={cx(className, s.container)}>
-      {entries.map((entry, i) => (
-        <div key={i} className={entry.correct ? s.correct : s.wrong}>
-          {entry.word} - {entry.correct ? "Correct" : "Wrong"}
-        </div>
-        )
-      )}
-    </div>
-);
+class SwipeHistory extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showingDefinition: false,
+    };
+
+    this.toggleShowDefinition = this.toggleShowDefinition.bind(this);
+  }
+
+  toggleShowDefinition() {
+    this.setState({
+      showingDefinition: !this.state.showingDefinition,
+    });
+  }
+
+  render() {
+    const showingDefinition = this.state.showingDefinition;
+
+    return(
+      <div className={cx(this.props.className, s.container)}>
+        <a href="#" onClick={this.toggleShowDefinition}>{showingDefinition ? "Hide Definitions" : "Show Definitions"}</a>
+        {this.props.entries.map((entry, i) => (
+            <div key={i} className={entry.correct ? s.correct : s.wrong}>
+              <b>{entry.word}</b>{showingDefinition ? " - " + entry.definition : ""}
+            </div>
+          )
+        )}
+      </div>
+    )
+  }
+}
 
 SwipeHistory.propTypes = {
   className: PropTypes.string,
